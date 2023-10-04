ad56dbcca0e3250c8c1e81df3b2ad9691b203756
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2023-10-04 09:39:44 -0400

    added shopping list project

--- workbook-1/ShoppingList/src/main/java/com/pluralsight/ShoppingList.java
+++ workbook-1/ShoppingList/src/main/java/com/pluralsight/ShoppingList.java
@@ -3,7 +3,7 @@ package com.pluralsight;
 public class ShoppingList {
     public static void main(String[] args) {
         System.out.println("This is my Shopping List:");
-        System.out.println("This is my Shopping List:");
+        System.out.println("monitor");
         System.out.println("This is my Shopping List:");
         System.out.println("This is my Shopping List:");
         System.out.println("This is my Shopping List:");
