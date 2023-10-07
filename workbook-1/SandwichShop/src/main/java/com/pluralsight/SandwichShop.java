eccb0e5266f5730ae07bcf76a58a559171f87905
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2023-10-07 13:38:04 -0400

    sandwich shop

---
+++ workbook-1/SandwichShop/src/main/java/com/pluralsight/SandwichShop.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class SandwichShop {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("what baconeggncheese u want? i got regular and the ockyway?");
+    }
+}
