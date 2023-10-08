b4b1904ba0757bcb013b4a3e6f7b9f250ca365af
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2023-10-07 21:27:33 -0400

    financial calculator

--- workbook-1/BasicCalculator/src/main/java/com/pluralsight/BasicCalculator.java
+++ workbook-1/BasicCalculator/src/main/java/com/pluralsight/BasicCalculator.java
@@ -1,2 +1,14 @@
-package com.pluralsight;public class BasicCalculator {
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        int num1 = myScanner.nextInt();
+        int num2 = myScanner.nextInt();
+
+        System.out.println(num1);
+        System.out.println(num2);
+    }
 }
