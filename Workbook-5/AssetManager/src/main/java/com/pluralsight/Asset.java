ed27b18555a7e823c7843dc5385dd4aaa77355fa
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2023-11-08 14:14:20 -0500

    Polymorphism exercise done

--- Workbook-5/AssetManager/src/main/java/com/pluralsight/Asset.java
+++ Workbook-5/AssetManager/src/main/java/com/pluralsight/Asset.java
@@ -1,6 +1,6 @@
 package com.pluralsight;
 
-public class Asset {
+public abstract class Asset {
     String description;
     String dateAcquired;
     double originalCost;
--- Workbook-5/AssetManager/src/main/java/com/pluralsight/Main.java
+++ Workbook-5/AssetManager/src/main/java/com/pluralsight/Main.java
@@ -3,5 +3,8 @@ package com.pluralsight;
 public class Main {
     public static void main(String[] args) {
 
+        Cash cash = new Cash("cash", "today", 55);
+
+        System.out.println( cash.getValue());
     }
 }
